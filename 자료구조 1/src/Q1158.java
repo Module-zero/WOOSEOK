17a23176331deb5784550ea11e2cb76a46467668
WOOSERK <syeokim@naver.com>
2020-10-06 01:23:37 +0900

    solve 1406, 10845, 1158, 10866

---
+++ 자료구조 1/src/Q1158.java
@@ -0,0 +1,73 @@
+/*
+문제
+요세푸스 문제는 다음과 같다.
+
+1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다. 예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+
+N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N과 K가 빈 칸을 사이에 두고 순서대로 주어진다. (1 ≤ K ≤ N ≤ 5,000)
+
+출력
+예제와 같이 요세푸스 순열을 출력한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * Created by WOOSERK.
+ * User: WOOSERK
+ * Date: 2020-10-05
+ * Time: 오후 11:06
+ */
+
+public class Q1158
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Queue answer = new LinkedList<>();
+
+        Queue queue = new LinkedList<>();
+
+        for(int i=1; i<=N; i++)
+        {
+            queue.offer(i);
+        }
+
+        while(N-- > 0)
+        {
+            int cnt = (K-1) % (N+1);
+
+            for(int i=0; i<cnt; i++)
+            {
+                queue.offer(queue.poll());
+            }
+
+            answer.offer(queue.poll());
+        }
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("<");
+        sb.append("" + answer.poll());
+
+        while(!answer.isEmpty())
+        {
+            sb.append(", " + answer.poll());
+        }
+
+        sb.append(">");
+
+        System.out.println(sb.toString());
+    }
+}
