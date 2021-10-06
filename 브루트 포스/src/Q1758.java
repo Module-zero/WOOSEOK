6137fe2ef8cb7379e0def4fe0443899f4571d8be
WOOSERK <wooserknim@gmail.com>
2021-10-06 20:27:10 +0900

    2021-10-05 algorithm

---
+++ 브루트 포스/src/Q1758.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by WOOSERK.
+ * User: WOOSERK
+ * Date: 2021-10-06
+ * Time: 오전 3:15
+ */
+
+public class Q1758
+{
+    static int[] ary = new int[100001];
+    static int[] tmp = new int[100001];
+
+    public static void sort(int start, int end)
+    {
+        if(start >= end)
+            return;
+
+        int mid = (start + end) / 2;
+
+        sort(start, mid);
+        sort(mid+1, end);
+        merge(start, end);
+    }
+
+    public static void merge(int start, int end)
+    {
+        int mid = (start + end) / 2;
+
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        while(i <= mid && j <= end)
+        {
+            if(ary[i] > ary[j])
+                tmp[k++] = ary[i++];
+            else
+                tmp[k++] = ary[j++];
+        }
+
+        while(i <= mid)
+            tmp[k++] = ary[i++];
+        while(j <= end)
+            tmp[k++] = ary[j++];
+
+        for(int a=start; a<=end; a++)
+            ary[a] = tmp[a-start];
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++)
+            ary[i] = Integer.parseInt(br.readLine());
+
+        sort(0, N-1);
+
+        long ans = 0;
+        for(int i=0; i<N; i++)
+        {
+            if(ary[i] - i <= 0)
+                continue;
+
+            ans += ary[i] - i;
+        }
+
+        System.out.println(ans);
+    }
+}
