c48dd44ece684a4b7eaffe24d475b252334845d7
WOOSERK <wooserknim@gmail.com>
2021-10-11 23:54:06 +0900

    2021-10-11 algorithm

---
+++ 브루트 포스/src/Q20551.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Created by WOOSERK.
+ * User: WOOSERK
+ * Date: 2021-10-11
+ * Time: 오후 11:45
+ */
+
+public class Q20551
+{
+    static int[] A = new int[200001];
+    static int[] tmp = new int[200001];
+    public static void sort(int start, int end)
+    {
+        if(start >= end)
+            return;
+
+        int mid = (start + end) / 2;
+
+        sort(start, mid);
+        sort(mid+1, end);
+        merge(start, end);
+    }
+
+    public static void merge(int start, int end)
+    {
+        int mid = (start + end) / 2;
+
+        int i = start;
+        int j = mid+1;
+        int k = 0;
+
+        while(i <= mid && j <= end)
+        {
+            if(A[i] < A[j])
+                tmp[k++] = A[i++];
+            else
+                tmp[k++] = A[j++];
+        }
+
+        while(i <= mid)
+            tmp[k++] = A[i++];
+        while(j <= end)
+            tmp[k++] = A[j++];
+
+        for(int a=start; a<=end; a++)
+            A[a] = tmp[a-start];
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<N; i++)
+            A[i] = Integer.parseInt(br.readLine());
+
+        sort(0, N-1);
+        for(int i=)
+
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<M; i++)
+        {
+            int D = Integer.parseInt(br.readLine());
+
+        }
+
+        System.out.print(sb.toString());
+    }
+}
