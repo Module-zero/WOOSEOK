a154c1c183f67e8b263219a51f9a7927103a89b3
WOOSERK <syeokim@naver.com>
2021-10-31 18:21:36 +0900

    2021-10-31 algorithm

--- 브루트 포스/src/Q21315.java
+++ 브루트 포스/src/Q21315.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
@@ -12,9 +14,26 @@ import java.util.StringTokenizer;
 
 public class Q21315
 {
-    public static void foo(int i, int k)
+    public static void foo(int k, Queue<Integer> queue)
     {
+        Queue<Integer> tmp = new LinkedList<>();
 
+        int cnt = (int)Math.pow(2, k);
+        for(int i=0; i<cnt; i++)
+            tmp.add(queue.poll());
+
+        while(cnt > 1)
+        {
+            cnt /= 2;
+
+            for(int i=0; i<cnt; i++)
+                tmp.add(tmp.poll());
+
+            for(int i=0; i<cnt; i++)
+                queue.add(tmp.poll());
+        }
+
+        queue.add(tmp.poll());
     }
 
     public static void main(String[] args) throws IOException
@@ -27,5 +46,41 @@ public class Q21315
         for(int i=0; i<N; i++)
             ary[i] = Integer.parseInt(st.nextToken());
 
+        int[] lim = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+        for(int i=1; i<=9; i++)
+        {
+            if(lim[i] >= N)
+                break;
+
+            for(int j=1; j<=9; j++)
+            {
+                if(lim[j] >= N)
+                    break;
+
+                Queue<Integer> queue = new LinkedList<>();
+
+                for(int k=N; k>=1; k--)
+                    queue.add(k);
+
+                foo(i, queue);
+                foo(j, queue);
+
+                boolean isOk = true;
+                for(int k=N-1; k>=0; k--)
+                {
+                    if(ary[k] != queue.poll())
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+
+                if(isOk)
+                {
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
+        }
     }
 }
